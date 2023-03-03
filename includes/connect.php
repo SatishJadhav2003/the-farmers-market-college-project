28d593d50dbc0107e09989c43d43968f75f2a472
satish <sj8390613529@gmail.com>
2023-03-03 21:24:10 +0530

    added file to connect to server

---
+++ includes/connect.php
@@ -0,0 +1,17 @@
+<!-- This file is just for connecting to server -->
+
+<?php
+//  $server = "localhost:3307";
+//  $user = "root";
+//  $pass = "";
+//  $db = "online-vegetables";
+// $con = mysqli_connect($server,$user,$pass,$db);
+
+$con = new mysqli("localhost:3307", "root", "", "online-vegetables");
+
+if (!$con) {
+   die(mysqli_error($con));
+}
+mysqli_close($con);
+
+?>
