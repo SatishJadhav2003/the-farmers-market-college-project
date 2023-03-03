98e1b4c0c1e4c51310591c1323036b253891d94f
satish <sj8390613529@gmail.com>
2023-03-03 11:02:11 +0530

    created file for insert brands

---
+++ admin/insert_brands.php
@@ -0,0 +1,10 @@
+<form action="" method="post" class="mb-2">
+    <div class="input-group mb-2 w-90 ">
+        <span class="input-group-text custom-navbar" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control" name="brands" placeholder="Insert Brand" aria-label="brands" aria-describedby="basic-addon1">
+    </div>
+
+    <div class="input-group mb-2 w-10 ">
+        <button class="custom-color p-2 my-3 border-0">Insert Brands</button>
+    </div>
+</form>
