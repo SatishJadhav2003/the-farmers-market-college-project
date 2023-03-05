d817265ec7a8955f7e6f41895a2157293ee98f12
satish <sj8390613529@gmail.com>
2023-03-05 08:36:45 +0530

    added new online server

--- includes/connect.php
+++ includes/connect.php
@@ -1,16 +1,23 @@
 <!-- This file is just for connecting to server -->
 
 <?php
-//  $server = "localhost:3307";
-//  $user = "root";
-//  $pass = "";
-//  $db = "online-vegetables";
-// $con = mysqli_connect($server,$user,$pass,$db);
+ $server = "sql12.freesqldatabase.com";
+ $user = "sql12602989";
+ $pass = "kYJmappSij";
+ $db = "sql12602989";
+$con = mysqli_connect($server,$user,$pass,$db);
 
-$con = new mysqli("localhost:3307", "root", "", "online-vegetables");
+// $con = new mysqli("localhost:3307", "root", "", "online-vegetables");
 
 if (!$con) {
    die(mysqli_error($con));
 }
 
 ?>
+
+
+<!-- Host: sql12.freesqldatabase.com
+Database name: sql12602989
+Database user: sql12602989
+Database password: kYJmappSij
+Port number: 3306 -->
